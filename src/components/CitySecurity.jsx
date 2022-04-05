494b2edf530349737b3aeb4e75e73b7fc91035db
Thasun05 <nicola.blackwolf05@gmail.com>
2022-04-05 13:00:33 +0200

    added municipality page and started working on city security

--- src/components/CitySecurity.jsx
+++ src/components/CitySecurity.jsx
@@ -1,5 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const CitySecurity = () => {};
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Card from "./Card";
+
+const CitySecurity = () => {
+    return(
+        <div className="city-secyrity">
+            <div className="background-wrapper">
+                <Navbar />
+                <div className="hero">
+                <div className="hero__text">
+                    <h1>Making your world the safest place to live.</h1>
+                    <p>The security of our city is founded on the common sense of the citizens, even though there will be a surveillance corp always supervising the city.</p>
+                </div>
+                <div className="hero__img">
+                    <img src="city.png" alt="city-security" className="hero__img" />
+                </div>
+            </div>
+            </div>
+            <div className="row">
+                <div className="row__card">
+                <Card 
+                className="row__card"
+                title="Good Behavior Bonus"
+                img="credit-card.png"
+                text="The good conduct of citizens will be rewarded with city credits, that can be used by the citizens themselves in shops and/or supermarkets. These credits can be included in your Curriculum Vitae (C.V.) or presented to your school/university to prove your social and civic collaboration."
+                />
+
+                </div>
+                <div className="row__card">
+                <Card 
+                className="row__card"
+                title="City Surveillance Body"
+                img="police.png"
+                text="The city’s surveillance corp is made of qualified police officers trained to ensure maximum security all around the city. All police officers will be periodically trained with updating courses in which new techniques and practices will be taught to ensure maximum safety."
+                />
+
+                </div>
+                <div className="row__card">
+                <Card 
+                className="row__card"
+                title="Eagle 360"
+                img="eagle.png"
+                text="Eagle 360 is an advanced city surveillance system that allows city authorities to supervise comfortably and safely every corner of the city. Citizens, by downloading an app, will be able to report any kind of infringement or crime to the authorities, actively contributing to the project."
+                />
+
+                </div>
+
+            </div>
+            <Footer />
+        </div>
+    );
+};
 
 export default CitySecurity;
--- src/components/Municipality.jsx
+++ src/components/Municipality.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Card from "./Card";
+import CitySecurity from "./CitySecurity";
 
 const Municipality = () => {
   return (
@@ -17,14 +19,14 @@ const Municipality = () => {
           <div className="hero__text">
             <h1>Municipal administrations of the future</h1>
             <p>
-              Bureaucracy and public administration: two words, two big
+              Burocracy and public administration: two words, two big
               problems. Today’s municipal systems present serious problems that
               cause a general slowdown of the public system, generating
               discontent within the population as well as continues delays and
               errors due to haste.
             </p>
             <button className="btn btn-hero">
-              <Link className="btn-hero-link" to="/goals">
+              <Link className="btn-hero-link" to="/city-security">
                 Our City Security Policy
               </Link>
             </button>
@@ -35,6 +37,32 @@ const Municipality = () => {
         </div>
       </div>
       {/* End Background Wrapper */}
+      <div className="goals municipality-cards">
+          <div className="goals__card">
+            <Card 
+            className="goals__card"
+            title="Innovative Learning"
+            img="brain.png"
+            text="Our city is going to offer a learning system to make everyone learn about technologies and new innovation. The mind of a man is the most precious part of him so making it grow is in our interest."
+            />
+          </div>
+          <div className="goals__card">
+            <Card 
+            className = "goals__card"
+            title="Digitalization"
+            img="virtual (1).png"
+            text="Burocracy is so annoying, so we tought about how to semplify it. The Answer is... DIGITALIZATION! We are thinking about a new way to manage public systems without burocracy and with sustainable methods. "
+          />
+          </div>
+      </div>
+      <div className="goals__card municipality-card-3">
+            <Card 
+              className= "goals__card"
+              title="Improved Infrastructures"
+              img="bridge (1).png"
+              text="To make our city better, our city is going to invest in infrastructures to let cityzens live their life much better. Submarine subway stations, high speed magnetic trains and 'smart' sidewalks. Better 'bridges' makes better people."
+            />
+          </div>
       <Footer />
     </div>
   );
