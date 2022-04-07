4d6fdf42a7d18a7ab1829b4818ba91dc00ce1e54
Matteo Sala <salazar34@outlook.it>
2022-04-07 20:29:45 +0200

    Adding dockerfiles and fixing cs street

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,50 +6,57 @@ import Navbar from "./Navbar";
 import Card from "./Card";
 
 const Projects = () => {
-    return (
-        <div className="Projects">
-            <div className="background-wrapper">
-                <Navbar />
-                <div className="hero">
-                    <div className="hero__text">
-                        <h1>You don't need to overthink. Just be smart</h1>
-                        <p>As well as the city, we have other project like the new way to move around the city, by sharing hydrogen cars.</p>
-                    </div>
-                    <div className="hero__img">
-                        <img src="idea.png" width="250" alt="project" className="hero__img" />
-                    </div>
-                </div>
-            </div>
-
-            <div className="row">
-                <div className="row__card">
-                    <Card
-                        className="row__card"
-                        title="No Pollution"
-                        img="car.png"
-                        text="lorem ipsum"
-                    />
-                </div>
-                <div className="row__card">
-                    <Card
-                        className="row__card"
-                        title="Renewable Energy"
-                        img="solar-energy.png"
-                        text="lorem ipsum"
-                    />
-                </div>
-                <div className="row__card">
-                    <Card
-                        className="row__card"
-                        title="Green Zones"
-                        img="river.png"
-                        text="lorem ipsum"
-                    />
-                </div>
-            </div>
-            <Footer />
+  return (
+    <div className="Projects">
+      <div className="background-wrapper">
+        <Navbar />
+        <div className="hero">
+          <div className="hero__text">
+            <h1>You don't need to overthink. Just be smart</h1>
+            <p>
+              As well as the city, we have other project like the new way to
+              move around the city, by sharing hydrogen cars.
+            </p>
+          </div>
+          <div className="hero__img">
+            <img
+              src="idea.png"
+              width="250"
+              alt="project"
+              className="hero__img"
+            />
+          </div>
         </div>
-    );
-}
-export default Projects;
+      </div>
 
+      <div className="row">
+        <div className="row__card">
+          <Card
+            className="row__card"
+            title="No Pollution"
+            img="car.png"
+            text="lorem ipsum"
+          />
+        </div>
+        <div className="row__card">
+          <Card
+            className="row__card"
+            title="Renewable Energy"
+            img="solar-energy.png"
+            text="lorem ipsum"
+          />
+        </div>
+        <div className="row__card">
+          <Card
+            className="row__card"
+            title="Green Zones"
+            img="river.png"
+            text="lorem ipsum"
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Projects;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import "./components/css/Main.css";
 
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = document.querySelector("#root");
+
+if (root) ReactDOM.createRoot(root).render(<App />);
