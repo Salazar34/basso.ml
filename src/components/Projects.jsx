66b3348cad38342f21525bff64f4700d8b83d948
Thasun05 <nicola.blackwolf05@gmail.com>
2022-04-08 12:55:44 +0200

    added text to projects cards, started working on a form

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,7 +35,7 @@ const Projects = () => {
             className="row__card"
             title="No Pollution"
             img="car.png"
-            text="lorem ipsum"
+            text="Our first tought when we decided to make this city, was how to reduce pollution to keep our world safe. One of the most sources of pollution is the smog created by cars and others veichle, so the solution we tought about is shared hydrogen cars. Hydrogen cars alone surely will reduce pollution, but using a system of car-sharing the pollution will be minimum."
           />
         </div>
         <div className="row__card">
@@ -43,7 +43,7 @@ const Projects = () => {
             className="row__card"
             title="Renewable Energy"
             img="solar-energy.png"
-            text="lorem ipsum"
+            text="Be renewable is our first goal, so having multiple sources of renewable energy is the only thing that our city is going to need to be auto-sustainable. Solar-energy, for the houses of the citizens. Hydroelectric and wind power for industries and thermic power for the city itself. Marco Schiavi best engineer we say, so he will be our hero.  "
           />
         </div>
         <div className="row__card">
@@ -51,10 +51,14 @@ const Projects = () => {
             className="row__card"
             title="Green Zones"
             img="river.png"
-            text="lorem ipsum"
+            text="Air is the most important thing keeping us alive, so we'll have great stretches of plants where citizens can walk to be in contact with nature. In the center of our city there will be a great common garden where people can grow their own plants. Our green zones are so important for us, that we will have a law against those who harm the enviroment.  "
           />
         </div>
       </div>
+      <label for="name" className="lbl in-lbl">Name: </label>
+
+      <input type="text" id="name" name="name" required
+        minlength="4" maxlength="8" size="100" className="in-lbl"></input>
       <Footer />
     </div>
   );
